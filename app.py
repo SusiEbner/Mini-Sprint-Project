fce7651fde6a7a9af4ddf90ec354b92ca9b59a02
lisakleinrubatscher <212666569+lisakleinrubatscher@users.noreply.github.com>
2025-06-04 14:31:44 +0200

    Update 1

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Hello, Streamlit")
+st.markdown("This is a simple **streamlit** app *to* demonstrate the 'basic' setup.")
+
+betreff = st.text_input("Task Betreff")
+st.slider("Task Priority", 0, 100, 50)
+st.checkbox("Task Completed")
+st.button("Submit")
+
+print(betreff)
