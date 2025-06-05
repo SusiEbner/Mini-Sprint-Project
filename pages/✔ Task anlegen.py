b9c677f43bc3c57e0a4f71b5dcc5328177b9dd5d
leaannadsr <leaannadosser05@gmail.com>
2025-06-05 11:36:27 +0000

    renaming

--- pages/✔ Task anlegen.py
+++ pages/✔ Task anlegen.py
@@ -1,4 +1,6 @@
 import streamlit as st
+st.title("Todo anlegen ")
+st.sidebar.success("")
 
 def add_task(self, title, due_date, label):
     st.session_state.tasks[title] = {"date": due_date, "label": label, "done": False}
