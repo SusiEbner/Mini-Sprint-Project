031b93bdbcf4a078f7a399ecc17b656b99459379
leaannadsr <leaannadosser05@gmail.com>
2025-06-05 12:07:03 +0000

    x

--- pages/✔ Task anlegen.py
+++ pages/✔ Task anlegen.py
@@ -1,9 +1,9 @@
 import streamlit as st
-
+import pandas as pd
 if "tasks" not in st.session_state:
-    st.session_state.tasks = {}
+    st.session_state['tasks'] = {}
 
-st.title("Todo anlegen ")
+st.title("Todo anlegen")
 
 def add_task(self, title, due_date, label):
     st.session_state.tasks[title] = {"date": due_date, "label": label, "done": False}
