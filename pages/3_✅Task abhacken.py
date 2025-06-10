1ae48b2e0ea7bdef149a74f5f2d88883e903c05d
elKlara <ek8594@mci4me.at>
2025-06-10 06:44:59 +0000

    update 3

--- pages/3_✅Task abhacken.py
+++ pages/3_✅Task abhacken.py
@@ -7,7 +7,7 @@ if "tasks" not in st.session_state:
 st.title("ToDo abhacken")
 
 if st.session_state['tasks']:
-    task_name = st.selectbox("Wähle eine Aufgabe zum Abhaken", list(st.session_state['tasks'].keys()), key="task_select")
+    task_name = st.checkbox("Wähle eine Aufgabe zum Abhaken", list(st.session_state['tasks'].keys()), key="task_select")
 
     if st.button("Aufgabe abhaken"):
         if task_name in st.session_state['tasks']:
